327b8bf8e2b656550671f63ad53c12269ca70bd9
Fabio Chippari <chipparidev@gmail.com>
2025-09-07 15:07:16 -0300

    Learned Print() and String Manipulation.

--- Day 1/task/task.py
+++ Day 1/task/task.py
@@ -1,2 +1,14 @@
-# Write your code below this line Teste 👇
-print("Hello World!")
+# Write your code below this line 👇
+
+# - Day 1 --------------------------------------------------------------------------------------------------------------
+# -- 1. Printing on console using print():
+print("1. Printing on console:")
+print("Hello world!")
+
+# -- 2. String Manipulation with "\n":
+print("\n2. String Manipulation:")
+print("Hello World!\nHello World!\nHello World!")
+
+# -- 3. Inputs on console.
+
+# ----------------------------------------------------------------------------------------------------------------------
