5cb2e190d61ad9de2168747a353c631fb72b658c
Fabio Chippari <chipparidev@gmail.com>
2025-09-07 23:46:34 -0300

    Learned Type Error and Type Checking.

--- Day 1/task/task.py
+++ Day 1/task/task.py
@@ -1,14 +1,7 @@
 
 # - Day 1 --------------------------------------------------------------------------------------------------------------
-# - 6. Variables -------------------------------------------------------------------------------------------------------
-# -- 6.1 Create a greeting:
-print("Welcome to the Band Name Generator!")
-
-# -- 6.2 Ask the user and store in a variable:
-city = input("What is your city name?\n")
-pet_name = input("What is your pet name?\n")
-
-# -- 6.3 Combine the variables to show their band name:
-print("Your band name could be " + city + " " + pet_name)
+# - 1. Printing --------------------------------------------------------------------------------------------------------
+print("- 1. Printing:")
+print("Hello world!")
 
 # ----------------------------------------------------------------------------------------------------------------------
--- Day 2/Type Error, Checking and Conversion/task.py
+++ Day 2/Type Error, Checking and Conversion/task.py
@@ -1,1 +1,27 @@
-len(12345)
+
+# - Day 2 --------------------------------------------------------------------------------------------------------------
+# - 2. Type Error ------------------------------------------------------------------------------------------------------
+# -- 2.1 Fix the len() function:
+# Before:
+# "len(12345)" - len() function doesn't accept int.
+
+# After:
+len("12345")
+
+# -- 2.2 Type Checking:
+# Description: You can check the data type of any value or variable in python using the type() function.
+
+# String Type Checking:
+print(type("Hello"))
+
+# Int Type Checking:
+print(type(123))
+
+# Float Type Checking:
+print(type(12.34))
+
+# Boolean Type Checking:
+print(type(True))
+
+
+# ----------------------------------------------------------------------------------------------------------------------
