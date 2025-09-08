f0add9389630f478c870844c518d1f48b1a545a0
Fabio Chippari <chipparidev@gmail.com>
2025-09-08 11:26:48 -0300

    Learned Mathematical Operations in Python.

--- Day 2/Mathematical Operations/task.py
+++ Day 2/Mathematical Operations/task.py
@@ -1,1 +1,52 @@
-print("My age: " + str(12))
+
+# - Day 2 --------------------------------------------------------------------------------------------------------------
+# - 3. Mathematical Operations -----------------------------------------------------------------------------------------
+print("- 3. Mathematical Operations")
+
+# Description:
+# To Add -> use "+" (plus) symbol.
+# To Subtract -> use "-" (minus) symbol.
+# To Multiple -> use "*" (asterisk) symbol.
+# To Divide -> use "/" (slash) symbol.
+# To Exponentiate -> use "**" (double asterisk) symbol.
+# To Floor Divide -> use "//" (double slash) symbol.
+# To Modulus -> use "%" (percentage) symbol.
+
+# Attention:
+# In Python the Mathematical Operations use PEMDAS, in which is basically states the order of priority (starting from left to right)
+# is Parentheses, Exponents, Multiplication or Division, Addition or Subtraction.
+
+# -- 3.1. Basic Operators:
+print("-- 3.1. Basic Operators:")
+
+# Addiction Operation:
+print("Addiction Operation -> 5 + 2 =", (5 + 2))
+
+# Subtraction Operation:
+print("Subtraction Operation -> 5 - 2 =", (5 - 2))
+
+# Multiplication Operation:
+print("Multiplication Operation -> 5 * 2 =", (5 * 2))
+
+# Division Operation:
+print("Division Operation -> 5 / 2 =", (5 / 2))
+
+# Exponentiation Operation:
+print("Exponentiation Operation -> 5 ** 2 =", (5 ** 2))
+
+# Floor Division Operation:
+print("Floor Division Operation -> 5 // 2 =", (5 // 2))
+
+# Modulus Operation:
+print("Modulo Operation -> 5 % 2 =", (5 % 2))
+
+# -- 3.2. What is the output of this code?
+print("\n-- 3.2. What is the output of this code?")
+print("3 * 3 + 3 / 3 - 3 =", (3 * 3 + 3 / 3 - 3))
+
+# -- 3.3. Change the code so it outputs 3?
+print("\n-- 3.3. Change the code so it outputs 3?")
+print("Before: 3 * 3 + 3 / 3 - 3 =", (3 * 3 + 3 / 3 - 3))
+print("After: 3 * (3 + 3) / 3 - 3 =", (3 * (3 + 3) / 3 - 3))
+
+# ----------------------------------------------------------------------------------------------------------------------
