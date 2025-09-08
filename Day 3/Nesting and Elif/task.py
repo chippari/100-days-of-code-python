4926bfdc6bfb40cf9bb8b5ef34c604f9d13f4d1b
Fabio Chippari <chipparidev@gmail.com>
2025-09-08 17:55:07 -0300

    Learned Nesting and Elif Statement.

--- Day 3/Nesting and Elif/task.py
+++ Day 3/Nesting and Elif/task.py
@@ -1,7 +1,27 @@
+
+# > Day 3 --------------------------------------------------------------------------------------------------------------
+# > 3. Nesting and Elif ------------------------------------------------------------------------------------------------
+print("> 3. Nesting and Elif")
+
+# >> 3.1. Nesting and Elif (Exercise 1) --------------------------------------------------------------------------------
+print(">> 3.1. Nesting and Elif (Exercise 1)")
+
 print("Welcome to the rollercoaster!")
 height = int(input("What is your height in cm? "))
 
 if height >= 120:
-    print("You can ride the rollercoaster")
+    print("You can ride the rollercoaster! :)")
+    age = int(input("What is your age? "))
+
+    if age <= 12:
+        print("Please pay $5.00")
+    elif age <= 18:
+        print("Please pay $7.00")
+    else:
+        print("Please pay $12.00")
 else:
-    print("Sorry you have to grow taller before you can ride.")
+    print("Sorry, you can't ride the rollercoaster! :(")
+
+# ----------------------------------------------------------------------------------------------------------------------
+
+
