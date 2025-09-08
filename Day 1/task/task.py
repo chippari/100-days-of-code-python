d724f20d992017623f6d3df04b52d58f2be6cd0a
Fabio Chippari <chipparidev@gmail.com>
2025-09-07 23:07:41 -0300

    Learned about Data Types.

--- Day 1/task/task.py
+++ Day 1/task/task.py
@@ -5,11 +5,8 @@
 print("Welcome to the Band Name Generator!")
 
 # -- 6.2 Ask the user and store in a variable:
-city = input("What is your city name? ")
-print(city)
-
-pet_name = input("What is your pet name? ")
-print(pet_name)
+city = input("What is your city name?\n")
+pet_name = input("What is your pet name?\n")
 
 # -- 6.3 Combine the variables to show their band name:
 print("Your band name could be " + city + " " + pet_name)
--- Day 2/Data Types/task.py
+++ Day 2/Data Types/task.py
@@ -1,1 +1,36 @@
-len("Hello")
+
+# - Day 2 --------------------------------------------------------------------------------------------------------------
+# - 1. Data Types ------------------------------------------------------------------------------------------------------
+# -- 1.1 Strings:
+# Definition: A String in Python is a sequence of characters enclosed in single (' '),
+# double (" "), or triple quotes (''' ''' or """ """). Strings are used to represent text.
+name = "Fabio"
+print(name)
+
+# -- 1.2 Integers:
+# Definition: An Integer (int) is a whole number without a decimal point.
+# It can be positive, negative, or zero.
+integer_number = 123
+print(integer_number)
+
+# You can use underscores (_) inside numbers to make them more readable.
+# This does not affect the actual value.
+large_integer_number = 123_456_789
+print(large_integer_number)
+
+# -- 1.3 Floats:
+# Definition: A Float (float) is a number that contains a decimal point or
+# is written in exponential (scientific) notation. Floats represent real numbers.
+float_number = 12.34
+print(float_number)
+
+# -- 1.4 Booleans:
+# Definition: A Boolean (bool) has only two possible values: True or False.
+# It is often used to represent the truth value of expressions and conditions.
+boolean_true = True
+print(boolean_true)
+boolean_false = False
+print(boolean_false)
+
+
+# ----------------------------------------------------------------------------------------------------------------------
