776d0cfb3d768183b9a1c18ead8f966f50e14f1a
Fabio Chippari <chipparidev@gmail.com>
2025-09-07 21:33:17 -0300

    Update task.py

--- Day 1/task/task.py
+++ Day 1/task/task.py
@@ -1,14 +1,20 @@
 # Write your code below this line 👇
 
 # - Day 1 --------------------------------------------------------------------------------------------------------------
-# -- 1. Printing on console using print():
-print("1. Printing on console:")
+# - 1. Printing --------------------------------------------------------------------------------------------------------
+print("- 1. Printing:")
 print("Hello world!")
 
-# -- 2. String Manipulation with "\n":
-print("\n2. String Manipulation:")
+# - 2. String Manipulation ---------------------------------------------------------------------------------------------
+print("\n- 2. String Manipulation:")
+
+# -- 2.1. String Manipulation using "\n":
+print("-- 2.1. String Manipulation using 'slash n':")
 print("Hello World!\nHello World!\nHello World!")
 
-# -- 3. Inputs on console.
+# -- 2.2. String Manipulation using Concatenation with "+" symbol:
+print("\n-- 2.2. String Manipulation using Concatenation with "+" symbol:")
+
+# - 3. Inputs ----------------------------------------------------------------------------------------------------------
 
 # ----------------------------------------------------------------------------------------------------------------------
