cd8ed30176a8f55fd0d73a5cdaa4180b053c8e5b
Fabio Chippari <chipparidev@gmail.com>
2025-09-08 17:33:07 -0300

    Learned If / Else Statements.

--- Day 3/If Else/task.py
+++ Day 3/If Else/task.py
@@ -1,2 +1,25 @@
+
+# > Day 3 --------------------------------------------------------------------------------------------------------------
+# > 1. If / Else (Conditional Statement) -------------------------------------------------------------------------------
+print("> 1. If / Else (Conditional Statement)")
+
+# Comparator Operators for If / Else Statements:
+# Greater than -> use ">" symbol.
+# Less than -> use "<" symbol.
+# Greater than or equal to -> use ">=" symbols.
+# Less than or equal to -> use "<=" symbols.
+# Equal to -> use "==" symbols.
+# Not equal to -> "!=" symbols.
+
+# >> 1.1. If / Else (Exercise) -----------------------------------------------------------------------------------------
+print(">> 1.1. If / Else (Exercise 1):")
+
 print("Welcome to the rollercoaster!")
 height = int(input("What is your height in cm? "))
+
+if height >= 120:
+    print("You can ride the rollercoaster! :)")
+else:
+    print("Sorry, you can't ride the rollercoaster! :(")
+
+# ----------------------------------------------------------------------------------------------------------------------
