c70a80567241327a76d3c5d6b003ebeee52a3fcd
Fabio Chippari <chipparidev@gmail.com>
2025-09-08 18:33:01 -0300

    Updated some things on Python Pizza Project.

--- Day 3/Python Pizza/task.py
+++ Day 3/Python Pizza/task.py
@@ -1,10 +1,7 @@
 
 # > Day 3 --------------------------------------------------------------------------------------------------------------
 # > 5. Python Pizza ----------------------------------------------------------------------------------------------------
-print("> 5. Python Pizza")
-
 # >> 5.1. Python Pizza (Exercise 1) ------------------------------------------------------------------------------------
-print("\n>> 5.1. Python Pizza (Exercise 1)")
 print("Welcome to Python Pizza Deliveries!")
 size = input("What size pizza do you want? S, M or L: ").upper()
 pepperoni = input("Do you want pepperoni on your pizza? Y or N: ").upper()
@@ -13,13 +10,10 @@ bill = 0
 
 if size == "S":
     bill += 15
-    print(f"Small Pizza cost ${bill}")
 elif size == "M":
     bill += 20
-    print(f"Medium Pizza cost ${bill}")
 elif size == "L":
     bill += 25
-    print(f"Large Pizza cost ${bill}")
 else:
     print("You typed something wrong!")
 
@@ -30,13 +24,11 @@ if pepperoni == "Y":
         pepperoni_price = 3
 
     bill += pepperoni_price
-    print(f"Peperoni Additional cost ${pepperoni_price}")
 
 if extra_cheese == "Y":
     extra_cheese_price = 1
     bill += 1
-    print(f"Extra Cheese cost ${extra_cheese_price}")
 
-print(f"Your final bill is: ${bill}")
+print(f"Your final bill is: ${bill}.")
 
 # ----------------------------------------------------------------------------------------------------------------------
