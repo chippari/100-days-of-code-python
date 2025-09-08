21f63190925b44b2d2a1d51d38dc327edfaffb20
Fabio Chippari <chipparidev@gmail.com>
2025-09-07 22:03:08 -0300

    Learning Inputs on Python.

--- Day 1/task/task.py
+++ Day 1/task/task.py
@@ -1,20 +1,6 @@
-# Write your code below this line 👇
 
 # - Day 1 --------------------------------------------------------------------------------------------------------------
-# - 1. Printing --------------------------------------------------------------------------------------------------------
-print("- 1. Printing:")
-print("Hello world!")
-
-# - 2. String Manipulation ---------------------------------------------------------------------------------------------
-print("\n- 2. String Manipulation:")
-
-# -- 2.1. String Manipulation using "\n":
-print("-- 2.1. String Manipulation using 'slash n':")
-print("Hello World!\nHello World!\nHello World!")
-
-# -- 2.2. String Manipulation using Concatenation with "+" symbol:
-print("\n-- 2.2. String Manipulation using Concatenation with "+" symbol:")
-
 # - 3. Inputs ----------------------------------------------------------------------------------------------------------
+
 
 # ----------------------------------------------------------------------------------------------------------------------
