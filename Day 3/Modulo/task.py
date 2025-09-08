7cc692900b1a004c2ae6818ba38cf414c114ced7
Fabio Chippari <chipparidev@gmail.com>
2025-09-08 17:45:01 -0300

    Learned Modulus Operator.

---
+++ Day 3/Modulo/task.py
@@ -0,0 +1,27 @@
+
+# > Day 3 --------------------------------------------------------------------------------------------------------------
+# > 2. Modulus Operator ------------------------------------------------------------------------------------------------
+print("> 2. Modulus Operator")
+# Description: The Modulus Operator gives you the remainder of a division.
+# Examples:
+# 6 % 2 = 0
+# 6 % 5 = 1
+# 6 % 4 = 2
+
+# >> 2.1. Modulus Operator (Exercise 1) --------------------------------------------------------------------------------
+print(">> 2.1. Modulus Operator (Exercise 1):")
+print("What is 10 % 3?")
+print(f"10 % 3 = {10 % 3}")
+
+# >> 2.2. Modulus Operator (Exercise 2) --------------------------------------------------------------------------------
+print("\n>> 2.1. Modulus Operator (Exercise 2):")
+print("Check Odd or Even.")
+number = int(input("Enter a number: "))
+
+if number % 2 == 0:
+    print(f"The number {number} is even.")
+else:
+    print(f"The number {number} is odd.")
+
+
+# ----------------------------------------------------------------------------------------------------------------------
