5ca1d84a6199193e457b0c9fe7ee1a2caa9cb7a3
Fabio Chippari <chipparidev@gmail.com>
2025-09-10 08:17:59 -0300

    Finished Banker Roulette Project.

--- Day 4/Banker Roulette/task.py
+++ Day 4/Banker Roulette/task.py
@@ -1,1 +1,13 @@
+
+# > Day 4 --------------------------------------------------------------------------------------------------------------
+# > 3. Banker Roulette -------------------------------------------------------------------------------------------------
+import random
+print("> 3. Banker Roulette")
+
+print("\nWelcome to the Banker Roulette!")
+print("One person will be choose to pay the restaurant bill.")
+
 friends = ["Alice", "Bob", "Charlie", "David", "Emanuel"]
+print(f"The person who was chosen to pay the restaurant bill is: {random.choice(friends)}")
+
+# ----------------------------------------------------------------------------------------------------------------------
