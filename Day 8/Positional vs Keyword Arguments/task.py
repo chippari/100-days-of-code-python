acaf7dc03898c7e31367fabb560a6a25962970c9
Fabio Chippari <chipparidev@gmail.com>
2025-09-13 18:58:42 -0300

    Learned about Function with Multiple Inputs.

--- Day 8/Positional vs Keyword Arguments/task.py
+++ Day 8/Positional vs Keyword Arguments/task.py
@@ -1,8 +1,26 @@
-# Functions with input
 
-def greet_with_name(name):
-    print(f"Hello {name}")
-    print(f"How do you do {name}?")
+# > Day 8 --------------------------------------------------------------------------------------------------------------
+# > 2. Positional vs Keyword Arguments ---------------------------------------------------------------------------------
+print("\n> 2. Positional vs Keyword Arguments")
 
+def greet_with(name, location):
+    print(f"-> Hello {name}, it's a pleasure to meet you!")
+    print(f"-> You live in {location}.")
 
-greet_with_name("Jack Bauer")
+# >> 2.1. Creating a Function with Multiple Inputs (Positional Argument) -----------------------------------------------
+print(">> 2.1. Creating a Function with Multiple Inputs (Positional Argument):")
+
+# By default, when you create a function in Python, it will keep the order of arguments in the function definition, and
+# this is called Positional Argument.
+
+greet_with("Fabio", "Sao Paulo")
+
+# >> 2.2. Creating a Function with Multiple Inputs (Keyword Argument) --------------------------------------------------
+print("\n>> 2.2. Creating a Function with Multiple Inputs (Keyword Argument):")
+
+# You can use keywords when you provide the arguments when you call a function so that there is less confusion which
+# value is assigned to which input parameter.
+
+greet_with(location="Sao Paulo", name="Fabio")
+
+# ----------------------------------------------------------------------------------------------------------------------
