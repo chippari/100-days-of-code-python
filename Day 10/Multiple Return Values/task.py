d465e1b03f8a22d5fb7ff712a8f02e72ca2eddf3
Fabio Chippari <chipparidev@gmail.com>
2025-10-05 20:36:05 -0300

    Learned about Multiple Return Value.

--- Day 10/Multiple Return Values/task.py
+++ Day 10/Multiple Return Values/task.py
@@ -1,7 +1,20 @@
+
+# > Day 10 -------------------------------------------------------------------------------------------------------------
+# > 2. Multiple Return Value -------------------------------------------------------------------------------------------
+print("\n> 2. Multiple Return Value")
+
+# > 2.1. Creating Functions with Multiple Return Value -----------------------------------------------------------------
+print("> 2.1. Creating Functions with Multiple Return Value")
+
 def format_name(f_name, l_name):
     formated_f_name = f_name.title()
     formated_l_name = l_name.title()
-    return f"{formated_f_name} {formated_l_name}"
+    return f'Your Formated Name is: {formated_f_name} {formated_l_name}'
 
+print("Welcome to Formated Name Program!")
+first_name = input('Enter your first name: ')
+last_name = input('Enter your last name: ')
 
-print(format_name("AnGEla", "YU"))
+print(format_name(f_name=first_name, l_name=last_name))
+
+# ----------------------------------------------------------------------------------------------------------------------
