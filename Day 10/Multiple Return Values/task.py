2920f5d5ad0bc9df344f74f169d5dddb8e5fb03d
Fabio Chippari <chipparidev@gmail.com>
2025-10-05 20:39:14 -0300

    Update task.py

--- Day 10/Multiple Return Values/task.py
+++ Day 10/Multiple Return Values/task.py
@@ -7,6 +7,9 @@ print("\n> 2. Multiple Return Value")
 print("> 2.1. Creating Functions with Multiple Return Value")
 
 def format_name(f_name, l_name):
+    if f_name == "" or l_name == "":
+        return "You didn't provide any values."
+
     formated_f_name = f_name.title()
     formated_l_name = l_name.title()
     return f'Your Formated Name is: {formated_f_name} {formated_l_name}'
