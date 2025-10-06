03264b5df9240fdc068be940e170cd6b477376ae
Fabio Chippari <chipparidev@gmail.com>
2025-10-06 20:09:09 -0300

    Learned about Docstrings.

--- Day 10/Docstrings/task.py
+++ Day 10/Docstrings/task.py
@@ -1,12 +1,17 @@
+
+# > Day 10 -------------------------------------------------------------------------------------------------------------
+# > 3. Docstrings ------------------------------------------------------------------------------------------------------
+print("\n> 3. Docstrings")
+
 def format_name(f_name, l_name):
+    """Take a first and last name and format it to return the title case version of the name."""
+
     formated_f_name = f_name.title()
     formated_l_name = l_name.title()
-    return f"{formated_f_name} {formated_l_name}"
+    return f'Your Formated Name is: {formated_f_name} {formated_l_name}'
 
-
-formatted_name = format_name("AnGeLa", "YU")
+formatted_name = format_name(f_name="FABIO", l_name="ChiPpaRi")
 
 length = len(formatted_name)
 
-
-
+# ----------------------------------------------------------------------------------------------------------------------
