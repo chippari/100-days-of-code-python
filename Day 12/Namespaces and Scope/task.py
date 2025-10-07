f77d217497e784490df3e7096d2da4697543d7a0
Fabio Chippari <chipparidev@gmail.com>
2025-10-07 20:10:00 -0300

    Learned about Namespace, Global and Local Scope.

--- Day 12/Namespaces and Scope/task.py
+++ Day 12/Namespaces and Scope/task.py
@@ -1,10 +1,17 @@
-enemies = 1
 
+# > Day 12 -------------------------------------------------------------------------------------------------------------
+# > 1. Namespaces, Global & Local Scope --------------------------------------------------------------------------------
+print("\n> 1. Namespaces, Global & Local Scope")
+
+# This variable is Global Scope, because we can access everywhere on the code.
+enemy = 1
 
 def increase_enemies():
+    # This variable is Local Scope, because we can access only inside this function.
     enemies = 2
     print(f"enemies inside function: {enemies}")
 
-
 increase_enemies()
-print(f"enemies outside function: {enemies}")
+print(f"enemies outside function: {enemy}")
+
+# ----------------------------------------------------------------------------------------------------------------------
