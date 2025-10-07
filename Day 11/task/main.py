ec2789eb3c0abe3221363f753a829b3daf4a75a2
Fabio Chippari <chipparidev@gmail.com>
2025-10-07 17:51:32 -0300

    Finished Blackjack Project.

--- Day 11/task/main.py
+++ Day 11/task/main.py
@@ -2,11 +2,12 @@
 # > Day 11 -------------------------------------------------------------------------------------------------------------
 # > 1. Blackjack Project -----------------------------------------------------------------------------------------------
 
-import random
+import random, art
 
 cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 
 def get_initial_cards(user_initial, computer_initial):
+    """Function to get initial cards."""
     # User get initial two cards.
     initial_two_cards = 0
     while initial_two_cards < 2:
@@ -21,6 +22,7 @@ def get_initial_cards(user_initial, computer_initial):
         computer_initial.append(random.choice(cards))
 
 def get_total_cards(player_cards):
+    """Function to get total cards."""
     # User get your total cards.
     player_total = 0
     index = 0
@@ -30,60 +32,62 @@ def get_total_cards(player_cards):
     return player_total
 
 def get_more_cards(player):
+    """Function to get more cards."""
     player.append(random.choice(cards))
     return player
 
 def play_again():
+    """Function to play again."""
     user_input = input('\nDo you want to play again? (Y/N): ').upper()
 
     if user_input == 'Y':
         blackjack_game()
     elif user_input == 'N':
         print('Thank you for playing!')
-        game_loop = False
 
 def blackjack_game():
+    """Function to play blackjack game."""
     user_cards = []
     computer_cards = []
-    user_total = 0
-    computer_total = 0
 
     get_initial_cards(user_cards, computer_cards)
     user_total = get_total_cards(user_cards)
+    computer_total = get_total_cards(computer_cards)
 
-    game_loop = True
-    while game_loop:
-        print(f'Your cards: {user_cards}, current score: {user_total}')
-        print(f'Computer cards: {computer_cards[0]}')
+    print(art.logo)
+    print(f'Your cards: {user_cards}, current score: {user_total}')
+    print(f'Computer cards: {computer_cards[0]}')
 
+    while True:
         ask_more_cards = input('Type (Y) to get another card, type (N) to pass: ').upper()
+
         if ask_more_cards == 'Y':
             get_more_cards(user_cards)
             user_total = get_total_cards(user_cards)
-
-            if user_total == 21:
-                print("You win!")
-                play_again()
-            elif user_total > 21:
-                print("You went over. You lose!")
-                play_again()
-
+            print(f'Your cards: {user_cards}, current score: {user_total}')
         elif ask_more_cards == 'N':
-            while computer_total <= 16:
-                get_more_cards(computer_cards)
-                computer_total = get_total_cards(computer_cards)
+            break
 
-            print(f'Your final hand: {user_cards}, final score: {user_total}')
-            print(f'Computer final hand: {computer_cards}, final score: {computer_total}')
+        if user_total == 21 or user_total > 21:
+            break
 
-            if user_total > computer_total or computer_total > 21:
-                print("You win!")
-            elif user_total == computer_total:
-                print("Draw!")
-            else:
-                print("You lose!")
+    while computer_total <= 16:
+        get_more_cards(computer_cards)
+        computer_total = get_total_cards(computer_cards)
 
-            play_again()
+    print(f'Your final hand: {user_cards}, final score: {user_total}')
+    print(f'Computer final hand: {computer_cards}, final score: {computer_total}')
+
+    if user_total > 21:
+        print("You went over. You lose!")
+    elif computer_total > user_total and computer_total <= 21:
+        print("You lose!")
+    elif user_total == computer_total:
+        print("Draw!")
+    else:
+        print("You win!")
+
+    play_again()
 
 blackjack_game()
 # ----------------------------------------------------------------------------------------------------------------------
