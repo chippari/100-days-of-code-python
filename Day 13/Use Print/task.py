0318c40e7ab24e6935992dd0d5a23f233657b4a7
Fabio Chippari <chipparidev@gmail.com>
2025-10-08 00:02:48 -0300

    Learned about Using the Debugger.

--- Day 13/Use Print/task.py
+++ Day 13/Use Print/task.py
@@ -8,5 +8,4 @@ word_per_page = int(input("Number of words per page: "))
 total_words = pages * word_per_page
 print(total_words)
 
-
 # ----------------------------------------------------------------------------------------------------------------------
