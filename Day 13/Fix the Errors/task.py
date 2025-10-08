9ba2e1ac64b7293a8b594b2daeb063e8dafdd7a6
Fabio Chippari <chipparidev@gmail.com>
2025-10-07 23:28:25 -0300

    Learned about Fixing Errors on the code.

--- Day 13/Fix the Errors/task.py
+++ Day 13/Fix the Errors/task.py
@@ -1,3 +1,16 @@
-age = int(input("How old are you?"))
+
+# > Day 13 -------------------------------------------------------------------------------------------------------------
+# > 4. Fix the errors --------------------------------------------------------------------------------------------------
+print("\n> 4. Fix the errors")
+
+try:
+    age = int(input("How old are you?"))
+except ValueError:
+    print("You have type in an invalid number. Please try again with a numerical response such as '15'.")
+    age = int(input("How old are you?"))
+
 if age > 18:
-print("You can drive at age {age}.")
+    print(f"You can drive at age {age}.")
+
+
+# ----------------------------------------------------------------------------------------------------------------------
