1f0e75b122deac4ddde6f34dd9b508b04d450d3f
Fabio Chippari <chipparidev@gmail.com>
2025-10-07 21:12:43 -0300

    Learned about Global Constant Variables.

---
+++ Day 12/Global Constants/task.py
@@ -0,0 +1,13 @@
+
+# > Day 12 -------------------------------------------------------------------------------------------------------------
+# > 4. Global Constants ------------------------------------------------------------------------------------------------
+print("\n> 4. Global Constants")
+
+# If you want to define a global constant variable, you need to define the variable name using UPPER CASE.
+# For examples:
+PI = 3.14
+GOOGLE_LINK = "https://www.google.com"
+
+print(PI, GOOGLE_LINK)
+
+# ----------------------------------------------------------------------------------------------------------------------
